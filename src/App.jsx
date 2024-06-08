f252f33775cdc9974488e9d4cfa45dae9443aff8
athomas227 <andrewthomas@pursuit.org>
2024-06-08 12:07:05 -0400

    edited app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './Components/Home/Home'
+import Navbar from './Components/Navbar/Navbar'
 
 function App() {
   return (
     <div>
       <Router>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
