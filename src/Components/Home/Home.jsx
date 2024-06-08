d0e44700429dd8d0c112d9af2716beba3556d22d
athomas227 <andrewthomas@pursuit.org>
2024-06-08 15:02:14 -0400

    added quiz to home component

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,7 +1,13 @@
+import React from 'react'
+import Quiz from '../Quiz/Quiz'
+
 export default function Home() {
+
     return (
         <div className="h-screen w-screen bg-gradient-to-b from-sky-700 to-sky-900 flex items-center justify-center">
-            <h1 className="text-4xl font-bold text-white">Home</h1>
+            <div>
+                <Quiz />
+            </div>
         </div>
     )
 }
