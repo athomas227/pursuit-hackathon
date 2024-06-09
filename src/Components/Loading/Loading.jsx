97ec330e315f8d9c24f0a1ae32c9a3cc2a7f28a8
athomas227 <andrewthomas@pursuit.org>
2024-06-09 16:07:07 -0400

    edited loading component and mood board

--- src/Components/Loading/Loading.jsx
+++ src/Components/Loading/Loading.jsx
@@ -3,6 +3,7 @@ import React from "react";
 export default function Loading() {
     return (
         <div>
+            <p class="text-center text-white font-extrabold">Loading your experience</p>
             <div class="bg-black bg-opacity-40 dark:bg-zinc-800 p-4 ring-1 ring-zinc-900/5 rounded-lg shadow-lg w-screen max-w-screen-sm">
                 <div class="flex-col space-y-4 animate-pulse">
                 <div class="rounded bg-zinc-200 dark:bg-zinc-700 w-1/4 aspect-square mx-auto"></div>
