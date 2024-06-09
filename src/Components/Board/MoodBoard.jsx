cece9b7b63f998dfad287e2b13b68efceea61d66
athomas227 <andrewthomas@pursuit.org>
2024-06-09 16:25:33 -0400

    edited moodboard

--- src/Components/Board/MoodBoard.jsx
+++ src/Components/Board/MoodBoard.jsx
@@ -6,7 +6,7 @@ const MoodBoard = () => {
   const { article } = useContext(ArticleContext);
 
   return (
-    <div className="relative h-screen w-screen overflow-hidden">
+    <div className="relative h-screen w-screen overflow-hidden pt-20">
       <video
         className="absolute top-0 left-0 w-full h-full object-cover"
         src="src/Components/Home/Untitled3.mov"
@@ -15,9 +15,10 @@ const MoodBoard = () => {
         loop
       ></video>
       <div className="absolute inset-0 bg-black opacity-30"></div>
-      <div className="relative z-10 flex flex-col items-center justify-center h-full w-full space-y-6">
-        <div className="text-white text-2xl font-bold"> Your Mood Board</div>
+      <div className="relative z-10 flex flex-col items-center justify-center h-full w-full space-y-6 pt-20 overflow-y-auto">
+        <div className="text-white text-2xl font-bold text-center mt-20"> Your Mood Board</div>
         <div className="bg-black bg-opacity-60 rounded-2xl mt-16 py-5 px-5">
+        
         <div className="w-full max-w-lg text-white">
           {article && article.length > 0 && (
               <MoodArticleItem article={article[0]} />
